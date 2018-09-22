21f3211e1c2cf31a4bace1b1e1b1d5a97d0e200f
Juyeong Lee <jlee668@unix1.csc.calpoly.edu>
2018-09-21 19:59:30 -0700

    completed assignment and commiting it

--- planets.py
+++ planets.py
@@ -1,6 +1,21 @@
+#
+# Name: Juyeong Lee
+# Student ID:
+# Date: September 21st, 2018
+#
+# Lab 0
+# Section 15
+# Purpose of the Lab:
+#    To learn how to use github functionality with file modificaiton.
+
 def weight_on_planets():
    # write your code here
    
+   weightEarth = float(input("What do you weigh on earth? "))
+   weightMars = weightEarth*0.38
+   weightJupiter = weightEarth*2.34
+
+   print("\nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds.".format(weightMars,weightJupiter))
    
    
 if __name__ == '__main__':
